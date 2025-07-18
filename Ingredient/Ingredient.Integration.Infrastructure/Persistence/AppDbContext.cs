ac5369714e1c7267f55d5213163a1cb7fb183c85
Morris Nkosi <nkosimorris7@gmail.com>
2025-07-18 09:08:00 +0200

    add db context

---
+++ Ingredient/Ingredient.Integration.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Ingredient.Integration.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ingredient.Integration.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Ingredient.Integration.Domain.Entities.Ingredient> Ingredients => Set<Ingredient.Integration.Domain.Entities.Ingredient>();
+        public DbSet<Recipe> Recipes => Set<Recipe>();
+        public DbSet<RecipeItem> RecipeItems => Set<RecipeItem>();
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RecipeItem>()
+            .HasKey(k => new { k.RecipeId, k.IngredientId });
+
+            modelBuilder.Entity<RecipeItem>()
+            .HasOne(k => k.Recipe)
+            .WithMany(r => RecipeItems)
+            .HasForeignKey(k => k.RecipeId);
+
+                        modelBuilder.Entity<RecipeItem>()
+            .HasOne(k => k.Ingredient)
+            .WithMany(r => RecipeItems)
+            .HasForeignKey(k => k.IngredientId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
